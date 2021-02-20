af50ec0c622aee37759a7f48cc7fc139010b4a25
SAIF ULLAH <mobinspire@MobInspires-MacBook-Pro.local>
2021-02-20 18:32:56 +0500

    project setup

---
+++ Trivis-iOS/Trivis-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trivis-iOS
+//
+//  Created by SAIF ULLAH on 20/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
